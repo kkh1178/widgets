377a5defbffd3d3bc2f03e173228facbd4388902
khill <kalisha.hill@gmail.com>
2020-07-28 12:22:05 -0500

    Added ability to open a link in another tab

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,11 @@ import React from 'react';
 const Link = ({className, href, children}) => {
     
     const onClick = (event) => {
+        // boolean properties to determine if a specific key is held down on mac or windows;
+        // If either of those cases are true, return early
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }   
         event.preventDefault();
         window.history.pushState({}, '', href);
         // Communicates to the route componenets that the url just changed
